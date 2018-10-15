cf6bf260b7a7174646fa71a817dfa21f294dad5d
Doug Greenwood <douglas_greenwood@hotmail.com>
2018-10-15 22:49:30 +0100

    End 07_04

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -4,31 +4,34 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MediaItemService {
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) {}
-
-get(medium) {
-  let getOptions = {
-    params: {
-      medium
-    }
-  };
-	return this.http.get<MediaItemsResponse>('mediaitems', getOptions).pipe(
-    map((response: MediaItemsResponse) => {
-      return response.mediaItems;
-    })
-    );
+  get(medium) {
+    let getOptions = {
+      params: { medium }
+    };
+    return this.http.get<MediaItemsResponse>('mediaitems', getOptions)
+      .pipe(
+        map((response: MediaItemsResponse) => {
+          return response.mediaItems;
+        })
+      );
+  }
+  
+  add(mediaItem) {
+    return this.http.post('mediaitems', mediaItem);
+  }
+  
+  delete(mediaItem) {
+    return this.http.delete(`mediaitems/${mediaItem.id}`);
+  }
 }
-add(mediaItem) {
-	return this.http.post('mediaitems', mediaItem);
-}
-delete(mediaItem) {
-	return this.http.delete(`mediaitems/${mediaItem.id}`);
+
+interface MediaItemsResponse {
+  mediaItems: MediaItem[]
 }
 
-
-
-interface MediaItem{
+interface MediaItem {
   id: number;
   name: string;
   medium: string;
@@ -36,8 +39,4 @@ interface MediaItem{
   year: number;
   watchedOn: number;
   isFavorite: boolean;
-}
-
-interface MediaItemsResponse {
-  mediaItems: MediaItem[];
 }
